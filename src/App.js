7304abefd653334d1c471cb3f857bbbcea08b3c8
tatianasnook <tatsnook@gmail.com>
2023-10-16 19:35:38 -0500

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,130 @@
-import logo from './logo.svg';
+import {Component} from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+class TimerInput extends Component {
+  render() {
+    return (
+      <div style={{ marginLeft: 100 }}>
+        <h3>Input your desired time</h3>
+        <input
+          type="number"
+          value={this.props.value}
+          onChange={this.props.handleChange}
+          required
+        />
+      </div>
+    );
+  }
+}
+
+class StartButton extends Component {
+  render() {
+    return (
+      <div style={{ marginLeft: 130 }}>
+        <button
+          disabled={!this.props.value}
+          onClick={this.props.startCountDown}
+          style={{backgroundColor: 'green', color: 'black'}}
         >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+          Start
+        </button>
+      </div>
+    );
+  }
+}
+
+class Timer extends Component {
+  render() {
+    return (
+      <div>
+        <h1 style={{ fontSize: 100, marginLeft: 100 }}>
+          {this.props.value}:{this.props.seconds}
+        </h1>
+      </div>
+    );
+  }
+}
+class App extends Component {
+  constructor(props){
+    super(props);
+    this.state = {
+      seconds: '00',
+      value: '',
+      isClicked: false
+    };
+    
+    this.handleChange = this.handleChange.bind(this);
+    this.startCountDown = this.startCountDown.bind(this);
+    this.tick = this.tick.bind(this);
+  }
+
+  handleChange(event) {
+    this.setState({
+      value: event.target.value
+    });
+  }
+
+  tick() {
+    var min = Math.floor(this.secondsRemaining / 60);
+    var sec = this.secondsRemaining - min * 60;
+
+    this.setState({
+      value: min,
+      seconds: sec
+    });
+
+    if (sec < 10) {
+      this.setState({
+        seconds: this.state.seconds
+      });
+    }
+
+    if (min < 10) {
+      this.setState({
+        value: "0" + min
+      });
+    }
+
+    if ((min === 0) & (sec === 0)) {
+      clearInterval(this.intervalHandle);
+    }
+
+    this.secondsRemaining--;
+  }
+
+  startCountDown() {
+    this.intervalHandle = setInterval(this.tick, 1000);
+    let time = this.state.value;
+    this.secondsRemaining = time * 60;
+    this.setState({
+      isClicked: true
+    });
+  }
+
+  render() {
+    const clicked = this.state.isClicked;
+    if (clicked) {
+      return (
+        <div>
+          <Timer value={this.state.value} seconds={this.state.seconds} />
+        </div>
+      )
+    } else {
+      return (
+        <div>
+          <TimerInput
+              value={this.state.value}
+              handleChange={this.handleChange}
+          />
+          <Timer value={this.state.value} seconds={this.state.seconds} />
+          <StartButton
+            startCountDown={this.startCountDown}
+            value={this.state.value}
+          />
+        </div>
+      );
+    }
+  }
 }
 
 export default App;
